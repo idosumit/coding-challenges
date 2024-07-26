ef73e3c703f1d97a8e6164406b72bf2b32365175
idosumit <psumitcoding@gmail.com>
2024-07-26 17:44:24 +0900

    square every digit - javascript

--- Solutions/JavaScript/square-every-digit.js
+++ Solutions/JavaScript/square-every-digit.js
@@ -1,15 +1,19 @@
 /*
 Question from: https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
 Markdown version available in: ../Questions/square-every-digit.md
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1.
 */
 
-"use strict";
+'use strict';
 
 const squareDigits = (num) => {
-  if (!isFinite(num)) return;
-  let numToArr = Array.from(num.toString());
-  const squaredResult = numToArr.map((element) => element ** 2).join("");
-  return Number(squaredResult);
+	if (!isFinite(num)) return;
+	let numToArr = Array.from(num.toString());
+	const squaredResult = numToArr.map((element) => element ** 2).join('');
+	return Number(squaredResult);
 };
 
 console.log(squareDigits(3456));
---
+++ Solutions/JavaScript/vowel-count.js
@@ -0,0 +1,19 @@
+/*
+Question from: https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+Markdown version available in: ../Questions/vowel-count.md
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+'use strict';
+
+const getCount = (str) => {
+	return [...str].reduce((acc, curVal) => ('aeiouAEIOU'.includes(curVal) ? ++acc : acc), 0);
+};
+
+console.log(getCount('Rendezvous'));
+console.log(getCount('Tyrannosaurus'));
